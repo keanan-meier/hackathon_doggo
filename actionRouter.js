76e3079ce50f11210776aff944cf948430e3023f
hjkopytko <hjkopytko@gmail.com>
2017-09-30 07:02:34 -0600

    out with the old in with the new! removed mongo, added mysql

--- actionRouter.js
+++ actionRouter.js
@@ -3,7 +3,7 @@ var bodyParser =require('body-parser');
 var dc = require('./Services/DirectorRelated/directorController');
 var lc = require('./Services/LoginRelated/loginController');
 
-var dbUtil = require('./Utils/dbTools');
+//var dbUtil = require('./Utils/dbTools');
 var setRoutes = function(app){
   //ADD ACTION ROUTES HERE (POST)
 
@@ -17,7 +17,7 @@ var setRoutes = function(app){
   app.use(bodyParser.json());
 
 //db object
-  var db = dbUtil.getDb();
+  //var db = dbUtil.getDb();
 
   //LOGIN
   app.post('/performDirectorLogin', function(req, res) {
@@ -32,17 +32,6 @@ var setRoutes = function(app){
   app.post('/addDirector', function(req,res){
     dc.directorController('create',req.body.director);
   });
-
-  //READ DIRECTOR
-  app.get('/directors', function(req,res){
-    var cursor = db.collection('directors').find();
-    cursor.each(function(err,doc) {
-      if(doc!=null) {
-        res.send(doc.username);
-      } else {
-      }
-    });
-  })
 };
 
 module.exports = {
--- webRouter.js
+++ webRouter.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var dbTools = require('./Utils/dbTools');
+var connection = dbTools.dbConnection;
 
 var setRoutes = function(app){
-
   //SET STATIC for js/css/imgs
   app.use(express.static("public/assets/"));
 
@@ -25,9 +26,15 @@ var directors = [ director={name:"Bill",phone:"(306)737-0749", address:"123 Fake
 
   //VIEW DIRECTORS
   app.get('/viewdirectors', function(req, res){
-    res.render('viewDirectors', {data: directors});
-  });
-
+      connection.query('SELECT * FROM directors', function(err,data){
+        if(err){
+          throw err;
+        } else {
+          res.render('viewDirectors',{data: data});
+        }
+      });
+    //res.render('viewDirectors', {data: directors});
+});
   //ADD Director
   app.get('/add', function(req,res){
     res.render('addDirector');
