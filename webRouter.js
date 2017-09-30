b4e39803fb49f52c6235548a71d6acb15eaf804b
keanan-meier <keanan.s.meier@gmail.com>
2017-09-30 12:05:31 -0600

    added doggo page

--- webRouter.js
+++ webRouter.js
@@ -24,23 +24,36 @@ var setRoutes = function(app){
 //               ]
 
   //place holder data
-  var directors = [ director={username:"billiyboy",name:"Bill",phone:"(306)737-0749", address:"123 Fake St."},
-                  director={username:"test84923",name:"Ted",phone:"(306)737-0749", address:"123 Fake St."},
-                  director={username:"frank849302",name:"Frank",phone:"(306)737-0749", address:"123 Fake St."},
-                  director={username:"kate8392",name:"Kate",phone:"(306)737-0749", address:"123 Fake St."}
-                ];
+  // var directors = [ director={username:"billiyboy",name:"Bill",phone:"(306)737-0749", address:"123 Fake St."},
+  //                 director={username:"test84923",name:"Ted",phone:"(306)737-0749", address:"123 Fake St."},
+  //                 director={username:"frank849302",name:"Frank",phone:"(306)737-0749", address:"123 Fake St."},
+  //                 director={username:"kate8392",name:"Kate",phone:"(306)737-0749", address:"123 Fake St."}
+  //               ];
+  //
+  // var fosters = [ foster={username:"tomtomtom32",name:"Tommy",phone:"(306)737-0749", address:"123 Fake St."},
+  //                 foster={username:"linda48932",name:"Linda",phone:"(306)737-0749", address:"123 Fake St."},
+  //                 foster={username:"marshall84903",name:"Marshall",phone:"(306)737-0749", address:"123 Fake St."},
+  //                 foster={username:"lauren8932",name:"Lauren",phone:"(306)737-0749", address:"123 Fake St."}
+  //               ];
 
-  var fosters = [ foster={username:"tomtomtom32",name:"Tommy",phone:"(306)737-0749", address:"123 Fake St."},
-                  foster={username:"linda48932",name:"Linda",phone:"(306)737-0749", address:"123 Fake St."},
-                  foster={username:"marshall84903",name:"Marshall",phone:"(306)737-0749", address:"123 Fake St."},
-                  foster={username:"lauren8932",name:"Lauren",phone:"(306)737-0749", address:"123 Fake St."}
-                ];
+var dogs = [dog={id:"1001", name:"Toby",age:"2", breed:"Cockapoo"},
+                dog={id:"2002", gender:"Man", foster:"Homeless", name:"Meatball",age:"5", breed:"Bulldog"},
+                dog={id:"3003", name:"Oscar",age:"1", breed:"Boxer"},
+                dog={id:"4004", name:"Jack",age:"8", breed:"Black Lab"}
+            ];
 
-  var dogs = [dog={id:"1001", name:"Toby",age:"2", breed:"Cockapoo"},
-                  dog={id:"2002", name:"Meatball",age:"5", breed:"Bulldog"},
-                  dog={id:"3003", name:"Oscar",age:"1", breed:"Boxer"},
-                  dog={id:"4004", name:"Jack",age:"8", breed:"Black Lab"}
-              ];
+var updatesData = [update={type:"Medical Update", date:"9/30/2017", desc:"Meatball was hungover from drinking"},
+                            update={type:"Injury Report", date:"9/29/2017", desc:"Meatball fell over from drinking"},
+                            update={type:"Medicine", date:"9/28/2017", desc:"Meatball was prescribed vodka"},
+                            update={type:"Injury Report", date:"9/29/2017", desc:"Meatball fell over from drinking"},
+                            update={type:"Injury Report", date:"9/29/2017", desc:"Meatball fell over from drinking"},
+                            update={type:"Injury Report", date:"9/29/2017", desc:"Meatball fell over from drinking"},
+                            update={type:"Injury Report", date:"9/29/2017", desc:"Meatball fell over from drinking"},
+                            update={type:"Injury Report", date:"9/29/2017", desc:"Meatball fell over from drinking"}
+            ];
+
+var dogdata={dog:dogs[1] ,updates:updatesData};
+
 
   //VIEW FOSTERS
   app.get('/viewfosters', function(req, res){
@@ -68,7 +81,7 @@ var setRoutes = function(app){
   //INDV DOG
   app.get('/dog/:dogID', function(req, res){
     console.log(req.params.dogID);
-    res.render('dog');
+    res.render('dog', {data:dogdata});
   });
 
   //INDV FOSTER
