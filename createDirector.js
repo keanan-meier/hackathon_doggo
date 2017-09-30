60a80113a73ede930504142ca8d908db4813486c
hjkopytko <hjkopytko@gmail.com>
2017-09-29 21:58:47 -0600

    reading functionality to collection

--- createDirector.js
+++ createDirector.js
@@ -15,9 +15,28 @@ var insertDocument = function(db,callback) {
   });
 };
 
-MongoClient.connect(url,function(err,db) {
+// MongoClient.connect(url,function(err,db) {
+//   assert.equal(null,err);
+//   insertDocument(db,function() {
+//     db.close();
+//   });
+// });
+
+var listDirectors = function(db, callback) {
+  var cursor = db.collection('directors').find();
+  cursor.each(function(err,doc) {
+    assert.equal(err,null);
+    if (doc!= null) {
+      console.dir(doc);
+    }else {
+      callback();
+    }
+  });
+};
+
+MongoClient.connect(url, function(err,db) {
   assert.equal(null,err);
-  insertDocument(db,function() {
+  listDirectors(db, function() {
     db.close();
   });
 });
