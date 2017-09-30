fdb31fb9dfc9bee7dbdc0b04ea5805f76004112b
keanan-meier <keanan.s.meier@gmail.com>
2017-09-29 23:06:57 -0600

    added body parser, and performLogin action

--- actionRouter.js
+++ actionRouter.js
@@ -1,11 +1,21 @@
 var express = require('express');
+var bodyParser =require('body-parser');
 
 var setRoutes = function(app){
   //ADD ACTION ROUTES HERE (POST)
 
+  //SETUP PARSERS
+  app.use(bodyParser());
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+  app.use(bodyParser.json());
+
   //LOGIN
   app.post('/performLogin', function(req, res) {
-    console.log(req);
+    console.log(req.body.user);
   });
 
 };
