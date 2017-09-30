fb7c6d948e29f3246f15252fd8e4b4d3be04ee5e
hjkopytko <hjkopytko@gmail.com>
2017-09-30 08:45:56 -0600

    commit me

--- webRouter.js
+++ webRouter.js
@@ -18,7 +18,6 @@ var setRoutes = function(app){
     res.render('login');
   });
 
-<<<<<<< HEAD
 // var directors = [ director={name:"Bill",phone:"(306)737-0749", address:"123 Fake St."},
 //                 director={name:"Ted",phone:"(306)737-0749", address:"123 Fake St."},
 //                 director={name:"Frank",phone:"(306)737-0749", address:"123 Fake St."},
@@ -28,45 +27,6 @@ var setRoutes = function(app){
   //VIEW DIRECTORS
   app.get('/viewdirectors', function(req, res){
       connection.query('SELECT * FROM director', function(err,data){
-=======
-  //place holder data
-  var directors = [ director={username:"billiyboy",name:"Bill",phone:"(306)737-0749", address:"123 Fake St."},
-                  director={username:"test84923",name:"Ted",phone:"(306)737-0749", address:"123 Fake St."},
-                  director={username:"frank849302",name:"Frank",phone:"(306)737-0749", address:"123 Fake St."},
-                  director={username:"kate8392",name:"Kate",phone:"(306)737-0749", address:"123 Fake St."}
-                ];
-
-  var fosters = [ foster={username:"tomtomtom32",name:"Tommy",phone:"(306)737-0749", address:"123 Fake St."},
-                  foster={username:"linda48932",name:"Linda",phone:"(306)737-0749", address:"123 Fake St."},
-                  foster={username:"marshall84903",name:"Marshall",phone:"(306)737-0749", address:"123 Fake St."},
-                  foster={username:"lauren8932",name:"Lauren",phone:"(306)737-0749", address:"123 Fake St."}
-                ];
-
-  var dogs = [dog={id:"1001", name:"Toby",age:"2", breed:"Cockapoo"},
-                  dog={id:"2002", name:"Meatball",age:"5", breed:"Bulldog"},
-                  dog={id:"3003", name:"Oscar",age:"1", breed:"Boxer"},
-                  dog={id:"4004", name:"Jack",age:"8", breed:"Black Lab"}
-              ];
-
-  //VIEW DIRECTORS
-  app.get('/viewdirectors', function(req, res){
-<<<<<<< HEAD
-    res.render('viewDirectors', {data: directors});
-  });
-
-  //VIEW FOSTERS
-  app.get('/viewfosters', function(req, res){
-    res.render('viewFosters', {data: fosters});
-  });
-
-  //VIEW DOGS
-  app.get('/viewdogs', function(req, res){
-    res.render('viewDogs', {data: dogs});
-  });
-
-=======
-      connection.query('SELECT * FROM directors', function(err,data){
->>>>>>> 782b986b319c0ed2428559039cee488c7176cee0
         if(err){
           throw err;
         } else {
@@ -75,12 +35,13 @@ var setRoutes = function(app){
       });
     //res.render('viewDirectors', {data: directors});
 });
->>>>>>> 76e3079ce50f11210776aff944cf948430e3023f
   //ADD Director
   app.get('/add', function(req,res){
     res.render('addDirector');
   });
 };
+//
+
 module.exports = {
   setRoutes: setRoutes
 }
